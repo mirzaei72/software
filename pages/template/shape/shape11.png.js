2105868f3933166065ca00aa494d03197d1fa8c7
wayne chan <wayne@cse-soft.org>
2015-08-14 16:53:52 +0800

    0.1

--- pages/template/shape/shape11.png.js
+++ pages/template/shape/shape11.png.js
@@ -4,7 +4,7 @@
   
   if (taskCfg.svgTool) {
     var defId = apiNode.newMarker();
-    var sHtml = "<div class='svg-shape' _defs='" + defId + "' _config='small-default-large default-looser-impacted p1-p2-p3-p0 center-right-default' _widget=\"['rgb(51,51,51)','rgb(51,51,51)',0,3]\" _zindex='998' _left='" + taskCfg.mouseX + "' _top='" + taskCfg.mouseY + "' _width='60' _height='60'><div class='shape-canvas'></div><div class='rotate-txt S5'></div><div class='img-resource zero-area'><link rel='bookmark' type='text/xml' href='/software/slide11/$images/shape/point_node.svg.json' /></div></div>\n";
+    var sHtml = "<div class='svg-shape' _defs='" + defId + "' _config='small-default-large default-looser-impacted p1-p2-p3-p0 center-right-default' _widget=\"['rgb(51,51,51)','rgb(51,51,51)',0,1]\" _zindex='998' _left='" + taskCfg.mouseX + "' _top='" + taskCfg.mouseY + "' _width='60' _height='60'><div class='shape-canvas'></div><div class='rotate-txt S5'></div><div class='img-resource zero-area'><link rel='bookmark' type='text/xml' href='/software/slide11/$images/shape/point_node.svg.json' /></div></div>\n";
     apiNode.innerHTML = sHtml;
     apiNode.addSvgWidget();
   }
